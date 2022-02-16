9b5c7d6218b33af317f1597b51a7e00026b449f2
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2022-02-16 17:49:30 +0100

    Admin login

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const router = express.Router();
+
+router.all('*', (req, res, next) => {
+    if (!req.session.admin) {
+        res.redirect('login')
+        return
+    }
+    next()
+})
 
 /* GET home page. */
 router.get('/', (req, res) => {
+    console.log(req.session.admin)
     res.render('index', {
         title: 'Express'
     });
 });
+
+
 
 module.exports = router;
